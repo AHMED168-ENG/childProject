dae4cd9fd76b4828882bf24f0b085066998e6198
unknown <zaza909077777@gmail.com>
2022-09-06 16:47:30 +0200

    2 updae

--- seeders/20220813145450-addColumn.js
+++ seeders/20220813145450-addColumn.js
@@ -1,42 +1,40 @@
-const bcrypt = require("bcrypt");
+import { hashSync } from "bcrypt";
 
-module.exports = {
-    up: (queryInterface, Sequelize) => {
-        return queryInterface
-            .bulkInsert("users", [
+export function up(queryInterface, Sequelize) {
+    return queryInterface
+        .bulkInsert("users", [
+            {
+                name: "ahmed reda",
+                email: "ahmed@ahmed.com",
+                age: 22,
+                password: hashSync(
+                    "01024756410ahmed",
+                    parseInt(process.env.BCRYPT_SEKRET_KEY)
+                ),
+                gender: "domyate",
+                isAdmin: true,
+                isDoctor: true,
+                number: "1024756410",
+                active: true,
+                image: "https://res.cloudinary.com/ahmed-zakys/image/upload/v1662473127/children/users/ssssss_l3h6jm.png",
+                Disability: 1,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+        ])
+        .then(() => {
+            return queryInterface.bulkInsert("disabilitys", [
                 {
-                    name: "ahmed reda",
-                    email: "ahmed@ahmed.com",
-                    age: 22,
-                    password: bcrypt.hashSync(
-                        "01024756410ahmed",
-                        parseInt(process.env.BCRYPT_SEKRET_KEY)
-                    ),
-                    gender: "domyate",
-                    isAdmin: true,
-                    isDoctor: true,
-                    number: "1024756410",
+                    name: "عدم القدره علي نطق حرف الكاف",
+                    description: "عدم القدره علي نطق حرف الكاف",
+                    image: "https://res.cloudinary.com/ahmed-zakys/image/upload/v1662473296/children/disabilitys/ssssss_nakesd.png",
                     active: true,
-                    image: "https://res.cloudinary.com/ahmed-zakys/image/upload/v1662473127/children/users/ssssss_l3h6jm.png",
-                    Disability: 1,
                     createdAt: new Date(),
                     updatedAt: new Date(),
                 },
-            ])
-            .then(() => {
-                return queryInterface.bulkInsert("disabilitys", [
-                    {
-                        name: "عدم القدره علي نطق حرف الكاف",
-                        description: "عدم القدره علي نطق حرف الكاف",
-                        image: "https://res.cloudinary.com/ahmed-zakys/image/upload/v1662473296/children/disabilitys/ssssss_nakesd.png",
-                        active: true,
-                        createdAt: new Date(),
-                        updatedAt: new Date(),
-                    },
-                ]);
-            });
-    },
-    down: (queryInterface, Sequelize) => {
-        return queryInterface.bulkDelete("users", null, {});
-    },
-};
+            ]);
+        });
+}
+export function down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("users", null, {});
+}
