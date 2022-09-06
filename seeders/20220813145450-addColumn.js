dcdcc161e8480bedfb86af7ed4edeebe16c992de
unknown <zaza909077777@gmail.com>
2022-09-06 18:04:09 +0200

    2 update

--- seeders/20220813145450-addColumn.js
+++ seeders/20220813145450-addColumn.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface
-            .bulkInsert("disabilitys", [
+            .bulkInsert("disabilities", [
                 {
                     name: "عدم القدره علي نطق حرف الكاف",
                     description: "عدم القدره علي نطق حرف الكاف",
@@ -20,13 +20,12 @@ module.exports = {
                         email: "ahmed@ahmed.com",
                         age: 22,
                         password: bcrypt.hashSync("01024756410ahmed", 10),
-                        gender: "domyate",
+                        gender: true,
                         isAdmin: true,
                         isDoctor: true,
-                        number: "1024756410",
                         active: true,
                         image: "https://res.cloudinary.com/ahmed-zakys/image/upload/v1662473127/children/users/ssssss_l3h6jm.png",
-                        Disability: 1,
+                        Disability: [1],
                         createdAt: new Date(),
                         updatedAt: new Date(),
                     },
