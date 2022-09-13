1164516b1770fc0a3da9e5fd56dd97b9d2a26c49
unknown <zaza909077777@gmail.com>
2022-09-13 17:31:08 +0200

    2 update

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
 module.exports = {
     username: "doadmin",
     password: "AVNS_jrNQFGmBOi7ZEyoKoCN",
     database: "defaultdb",
     host: "db-postgresql-nyc1-90758-do-user-12428913-0.b.db.ondigitalocean.com",
     dialect: "postgres",
+    port: "25060",
     dialectOptions: {
         ssl: {
             rejectUnauthorized: false,
