f59e687591187042717186e1b181fb20ddabae93
unknown <zaza909077777@gmail.com>
2022-09-16 21:32:49 +0200

    2 update

--- router/backend/testIng.router.js
+++ router/backend/testIng.router.js
@@ -1,17 +1,17 @@
 const {
-  AllTestingController,
-  EditTestingController,
-  activeTesting,
-  deleteTesting,
-  EditTestingControllerPost,
-  addTestingController,
-  addTestingControllerPost,
-} = require("../../controller/backend/Testing");
+    AllTestingController,
+    EditTestingController,
+    activeTesting,
+    deleteTesting,
+    EditTestingControllerPost,
+    addTestingController,
+    addTestingControllerPost,
+} = require("../../controller/backend/testing");
 const { uploade_img, uploade_img_multi_fild } = require("../../Helper/helper");
 const { isAuthonticate } = require("../../middel_ware/backEnd/isAuthonticate");
 
 const {
-  TestingValidation,
+    TestingValidation,
 } = require("../../validation/backEnd/Testing.validation");
 
 const router = require("express").Router();
@@ -19,21 +19,21 @@ const router = require("express").Router();
 router.get("/AllTesting", isAuthonticate, AllTestingController);
 router.get("/addTesting", isAuthonticate, addTestingController);
 router.post(
-  "/addTesting",
-  isAuthonticate,
-  TestingValidation(),
-  addTestingControllerPost
+    "/addTesting",
+    isAuthonticate,
+    TestingValidation(),
+    addTestingControllerPost
 );
 router.get("/EditTesting/:id", isAuthonticate, EditTestingController);
 router.post(
-  "/EditTesting/:id",
-  isAuthonticate,
-  TestingValidation(),
-  EditTestingControllerPost
+    "/EditTesting/:id",
+    isAuthonticate,
+    TestingValidation(),
+    EditTestingControllerPost
 );
 router.get("/activeTesting/:id", isAuthonticate, activeTesting);
 router.post("/deleteTesting/:id", isAuthonticate, deleteTesting);
 
 module.exports = {
-  TestingRouter: router,
+    TestingRouter: router,
 };
