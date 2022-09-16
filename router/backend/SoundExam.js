0bca91b5438fd2ef691f61a6b8c264404640fa02
unknown <zaza909077777@gmail.com>
2022-09-16 21:36:00 +0200

    2 update

--- router/backend/SoundExam.js
+++ router/backend/SoundExam.js
@@ -1,12 +1,12 @@
 const {
-  addSoundController,
-  EditSoundController,
-  EditSoundControllerPost,
-  addSoundControllerPost,
-  AllSoundController,
-  activeSound,
-  deleteSound,
-} = require("../../controller/backend/SoundExam");
+    addSoundController,
+    EditSoundController,
+    EditSoundControllerPost,
+    addSoundControllerPost,
+    AllSoundController,
+    activeSound,
+    deleteSound,
+} = require("../../controller/backend/soundExam");
 const { uploade_img_multi_fild } = require("../../Helper/helper");
 const { isAuthonticate } = require("../../middel_ware/backEnd/isAuthonticate");
 
@@ -17,43 +17,43 @@ const router = require("express").Router();
 router.get("/AllSound", isAuthonticate, AllSoundController);
 router.get("/addSound", isAuthonticate, addSoundController);
 router.post(
-  "/addsound",
-  isAuthonticate,
-  uploade_img_multi_fild(
-    [
-      {
-        name: "image",
-      },
-      {
-        name: "pdf",
-      },
-    ],
-    "public/backEnd/assets/img/sound"
-  ),
-  GideValidation(),
-  addSoundControllerPost
+    "/addsound",
+    isAuthonticate,
+    uploade_img_multi_fild(
+        [
+            {
+                name: "image",
+            },
+            {
+                name: "pdf",
+            },
+        ],
+        "public/backEnd/assets/img/sound"
+    ),
+    GideValidation(),
+    addSoundControllerPost
 );
 router.get("/EditSound/:id", isAuthonticate, EditSoundController);
 router.post(
-  "/EditSound/:id",
-  isAuthonticate,
-  uploade_img_multi_fild(
-    [
-      {
-        name: "image",
-      },
-      {
-        name: "pdf",
-      },
-    ],
-    "public/backEnd/assets/img/sound"
-  ),
-  GideValidation(),
-  EditSoundControllerPost
+    "/EditSound/:id",
+    isAuthonticate,
+    uploade_img_multi_fild(
+        [
+            {
+                name: "image",
+            },
+            {
+                name: "pdf",
+            },
+        ],
+        "public/backEnd/assets/img/sound"
+    ),
+    GideValidation(),
+    EditSoundControllerPost
 );
 router.get("/activeSound/:id", isAuthonticate, activeSound);
 router.post("/deleteSound/:id", isAuthonticate, deleteSound);
 
 module.exports = {
-  soundExamRouter: router,
+    soundExamRouter: router,
 };
