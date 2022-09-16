9da978ed799d6789052a3455028b968241eae985
unknown <zaza909077777@gmail.com>
2022-09-16 21:34:32 +0200

    2 update

--- router/backend/gide.js
+++ router/backend/gide.js
@@ -1,18 +1,18 @@
 const {
-  AllGideController,
-  EditGideController,
-  activeGide,
-  deleteGide,
-  EditGideControllerPost,
-  addGideController,
-  addGideControllerPost,
-} = require("../../controller/backend/Gide");
+    AllGideController,
+    EditGideController,
+    activeGide,
+    deleteGide,
+    EditGideControllerPost,
+    addGideController,
+    addGideControllerPost,
+} = require("../../controller/backend/gide");
 const { uploade_img_multi_fild } = require("../../Helper/helper");
 const { isAuthonticate } = require("../../middel_ware/backEnd/isAuthonticate");
 
 const { GideValidation } = require("../../validation/backEnd/Gide.validation");
 const {
-  EditUserValidation,
+    EditUserValidation,
 } = require("../../validation/backEnd/user.validation");
 
 const router = require("express").Router();
@@ -20,43 +20,43 @@ const router = require("express").Router();
 router.get("/AllGide", isAuthonticate, AllGideController);
 router.get("/addGide", isAuthonticate, addGideController);
 router.post(
-  "/addGide",
-  isAuthonticate,
-  uploade_img_multi_fild(
-    [
-      {
-        name: "image",
-      },
-      {
-        name: "pdf",
-      },
-    ],
-    "public/backEnd/assets/img/gide"
-  ),
-  GideValidation(),
-  addGideControllerPost
+    "/addGide",
+    isAuthonticate,
+    uploade_img_multi_fild(
+        [
+            {
+                name: "image",
+            },
+            {
+                name: "pdf",
+            },
+        ],
+        "public/backEnd/assets/img/gide"
+    ),
+    GideValidation(),
+    addGideControllerPost
 );
 router.get("/EditGide/:id", isAuthonticate, EditGideController);
 router.post(
-  "/EditGide/:id",
-  isAuthonticate,
-  uploade_img_multi_fild(
-    [
-      {
-        name: "image",
-      },
-      {
-        name: "pdf",
-      },
-    ],
-    "public/backEnd/assets/img/gide"
-  ),
-  GideValidation(),
-  EditGideControllerPost
+    "/EditGide/:id",
+    isAuthonticate,
+    uploade_img_multi_fild(
+        [
+            {
+                name: "image",
+            },
+            {
+                name: "pdf",
+            },
+        ],
+        "public/backEnd/assets/img/gide"
+    ),
+    GideValidation(),
+    EditGideControllerPost
 );
 router.get("/activeGide/:id", isAuthonticate, activeGide);
 router.post("/deleteGide/:id", isAuthonticate, deleteGide);
 
 module.exports = {
-  GideRouter: router,
+    GideRouter: router,
 };
